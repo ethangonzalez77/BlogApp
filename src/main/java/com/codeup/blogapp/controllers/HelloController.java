d70b06001429dda878c740d19154e21c390d9d59
ethan <ethan.gonzalez00774@gmail.com>
2023-01-18 15:07:49 -0800

    notes were added in HelloController

--- src/main/java/com/codeup/blogapp/controllers/HelloController.java
+++ src/main/java/com/codeup/blogapp/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.codeup.blogapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
     //personal notes
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
         public String hello() {
             return "Hello from Spring!";
         }
-        
+
+
+        @GetMapping("/hello/{name}")//🍔1.the parameter {name} is set up in the url
+        @ResponseBody
+        public String sayHello(@PathVariable String name) {//🍔2. this method has the parameter set up as it usually is however, this time the @PathVariable is added so that the parameter in this method is connected to the parameter in url, also each parameter needs the @PathVariable annotation
+            return "Hello " + name + "!";
+        }
 
 
 
