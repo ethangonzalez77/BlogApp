b19c11ec06b7750046aca1cadf9fe7c75de446e7
ethan <ethan.gonzalez00774@gmail.com>
2023-01-19 23:11:59 -0800

    new setters

--- src/main/java/com/codeup/blogapp/controllers/PostController.java
+++ src/main/java/com/codeup/blogapp/controllers/PostController.java
@@ -44,39 +44,39 @@ public class PostController {
 
 
 
+    @GetMapping("/posts")
+    public String allPosts (Model model) {
 
-//    public String allPosts (Model model) {
-//
-//        List<Post> posts = new ArrayList<>();
-//        posts.add(new Post("TV", "Watch when you need a break."));
-//        posts.add(new Post("Coffee", "Drink when you feel low."));
-//        model.addAttribute("allPosts", posts);
-//
-//        return "posts/Index";
-//    }
-//
-//    @GetMapping("/posts/{id}")
-//    public String viewIndividualPost(@PathVariable long id, Model model) {
-//
-//        Post currentPost = new Post(id, "Car", "Takes us from A to B.");
-//        model.addAttribute("post", currentPost);
-//        return "posts/show";
-//    }
-//
-//
-//    @GetMapping("/posts/create")
-//    @ResponseBody
-//    public String getCreate() {
-//        return "Form for creating a post!";
-//    }
-//
-//
-//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String postCreate() {
-//        return "Posts the create a post page!";
-//    }
-//
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("TV", "Watch when you need a break."));
+        posts.add(new Post("Coffee", "Drink when you feel low."));
+        model.addAttribute("allPosts", posts);
+
+        return "posts/Index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String viewIndividualPost(@PathVariable long id, Model model) {
+
+        Post currentPost = new Post(id, "Car", "Takes us from A to B.");
+        model.addAttribute("post", currentPost);
+        return "posts/show";
+    }
+
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String getCreate() {
+        return "Form for creating a post!";
+    }
+
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String postCreate() {
+        return "Posts the create a post page!";
+    }
+
 
 
 
